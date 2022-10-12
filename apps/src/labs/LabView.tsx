43245479e17e2601dbaef0b6323cdc4a252de800
molly-moen <molly@code.org>
2022-10-12 09:53:41 -0700

    get data

--- apps/src/labs/LabView.tsx
+++ apps/src/labs/LabView.tsx
@@ -1,7 +1,13 @@
 import React, { FunctionComponent } from 'react';
+const getScriptData = require('../util/getScriptData');
 
 // ingest appOptions from backend into AppOptions type
 const LabView = () => {
+  const appOptions = getScriptData('appoptions');
+  console.log(appOptions);
+  const level = getScriptData('level');
+  console.log(level);
+
   return <div>Hello from lab lab</div>
 }
 
